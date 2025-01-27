d190847a11cefd0814489563e54e52a0cf4a5253
GomzaUlq <UlquiorraSin@yandex.ru>
2025-01-27 21:14:05 +0500

    Update user.py

--- TeaBlog/schemas/user.py
+++ TeaBlog/schemas/user.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
+from models.user import User
+
 
 class CreateUser(BaseModel):
     username: str
@@ -12,3 +14,16 @@ class CreateUser(BaseModel):
 class UpdateUser(BaseModel):
     username: Optional[str] = None
     first_name: Optional[str] = None
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None  # Изменено на Optional
+
+
+class UserInDB(User):
+    hashed_password: str  # Здесь не требуется изменений, но убедитесь, что User имеет правильные аннотации
